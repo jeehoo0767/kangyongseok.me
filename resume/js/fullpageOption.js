a30c89eb9b5936b24d5d0a29812c67da946e3433
CodeReading <kangyongsuek@gmail.com>
2019-01-18 10:36:49 +0900

    feat: 두번째 section 에 fullPage.js 가 가진 옵션중 slide 옵션 추가

--- resume/js/fullpageOption.js
+++ resume/js/fullpageOption.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
         menu: '#menu',
         autoScrolling:true,
         scrollHorizontally: true,
+        slidesNavigation: true,
+		slidesNavPosition: 'bottom',
         anchors:['Introduce', 'secondPage', 'thirdPage', 'fourPage'],
         sectionsColor: ['#22B473', '#4267B2',  '#F95759', '#f1f1f1'],
+        responsiveWidth: 700,
         afterLoad: function(origin) {
             if(origin === "secondPage") {
                 
